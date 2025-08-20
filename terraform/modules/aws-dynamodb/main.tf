5533b3210b9d484c0e2fc6115a9255a87bd07e34
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-08-20 09:37:38 +0900

    Added: terraform GSI support.

--- terraform/modules/aws-dynamodb/main.tf
+++ terraform/modules/aws-dynamodb/main.tf
@@ -15,5 +15,17 @@ resource "aws_dynamodb_table" "todo_table" {
     }
   }
 
+  dynamic "global_secondary_index" {
+    for_each = (var.range_key != null && var.index_name != null) ? [{
+      name : var.index_name,
+      hash_key : var.range_key,
+    }] : []
+    content {
+      name            = global_secondary_index.value["name"]
+      hash_key        = global_secondary_index.value["hash_key"]
+      projection_type = var.gsi_projection_type
+    }
+  }
+
   tags = var.tags
 }
--- terraform/modules/aws-dynamodb/variables.tf
+++ terraform/modules/aws-dynamodb/variables.tf
@@ -3,6 +3,12 @@ variable "table_name" {
   type        = string
 }
 
+variable "index_name" {
+  description = "DynamoDB GSI name."
+  type        = string
+  default     = null
+}
+
 variable "hash_key" {
   description = "DynamoDB table partition key."
   type        = string
@@ -12,6 +18,12 @@ variable "range_key" {
   description = "DynamoDB table sort key."
   type        = string
   default     = null
+}
+
+variable "gsi_projection_type" {
+  description = "DynamoDB GSI projection type."
+  type        = string
+  default     = "KEYS_ONLY"
 }
 
 variable "billing_mode" {
