81fa92ddf7c90c2f7d6160375ca212727e98aeb1
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-09-27 05:09:05 +0900

    Updated: get_rest_api_url.sh

--- tools/get_rest_api_url.sh
+++ tools/get_rest_api_url.sh
@@ -5,6 +5,7 @@
 JQ=${JQ:-jq}
 AWSCLI=${AWSCLI:-awslocal}
 AWS_REGION=${AWS_REGION:-ap-northeast-1}
+AWS_ENDPOINT=${AWS_ENDPOINT:localstack.localhost:4566}
 
 [ $# -lt 2 ] && echo "usage: $0 api_name stage_name [endpoint_path]" && exit 1
 
@@ -17,4 +18,4 @@ api_id=$(
   ${JQ} -r '.[0].id'
 )
 
-echo "http://${api_id}.execute-api.${AWS_REGION}.localstack.localhost:4566/${stage_name}${endpoint_path}"
+echo "http://${api_id}.execute-api.${AWS_REGION}.${AWS_ENDPOINT}/${stage_name}${endpoint_path}"
