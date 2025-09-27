ced8d108f45409a1d2d3e7638841a12f4acef896
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-09-27 23:24:13 +0900

    Changed: terraform replace condition.

--- terraform/modules/aws-apigateway/main.tf
+++ terraform/modules/aws-apigateway/main.tf
@@ -31,24 +31,14 @@ resource "aws_api_gateway_integration" "rest_api_integration" {
 resource "aws_api_gateway_deployment" "rest_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
 
-  triggers = {
-    # NOTE: The configuration below will satisfy ordering considerations,
-    #       but not pick up all future REST API changes. More advanced patterns
-    #       are possible, such as using the filesha1() function against the
-    #       Terraform configuration file(s) or removing the .id references to
-    #       calculate a hash against whole resources. Be aware that using whole
-    #       resources will show a difference after the initial implementation.
-    #       It will stabilize to only change when resources change afterwards.
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.rest_api_resource.id,
-      aws_api_gateway_method.rest_api_method.id,
-      aws_api_gateway_integration.rest_api_integration.id,
-      aws_api_gateway_rest_api_policy.rest_api_ip_restriction.id,
-    ]))
-  }
-
   lifecycle {
     create_before_destroy = true
+    replace_triggered_by = [
+      aws_api_gateway_resource.rest_api_resource,
+      aws_api_gateway_method.rest_api_method,
+      aws_api_gateway_integration.rest_api_integration,
+      aws_api_gateway_rest_api_policy.rest_api_ip_restriction
+    ]
   }
 }
 
